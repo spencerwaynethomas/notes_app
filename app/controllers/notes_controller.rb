2c48f4c24f13fb8be2de20b8b658ce24268ec8a8
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2019-11-12 07:05:29 -0700

    notes update

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -10,4 +10,26 @@ class NotesController < ApplicationController
   def new
     @note = Note.new
   end
+
+  def create
+    @note = Note.new(notes_params)
+
+    if @note.save
+      redirect_to notes_path
+    else 
+      render :new
+    end
+  end
+  def notes_params
+    params.require(:note).permit(:title, :author, :body)
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+
+
+
+
 end
